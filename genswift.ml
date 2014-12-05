697637079743952f0e3c3c84a02b9f2c4dfb4472
Rocky Duan <dementrock@gmail.com>
2014-12-05 11:58:06 +0100

    generics

--- genswift.ml
+++ genswift.ml
@@ -6,72 +6,16 @@ open Gencommon.SourceWriter
 
 exception SwiftEx of string
 
-let module_type_path t =
-		match t with
-		| TClassDecl cl ->
-			let (ns, n) = cl.cl_path in
-			(String.concat "." (ns @ [n]))
-		| TEnumDecl e ->
-			let (ns, n) = e.e_path in
-			(String.concat "." (ns @ [n]))
-		| TTypeDecl d ->
-			let (ns, n) = d.t_path in
-			(String.concat "." (ns @ [n]))
-		| TAbstractDecl a ->
-			let (ns, n) = a.a_path in
-			(String.concat "." (ns @ [n]))
-
-let module_type_type t =
-	match t with
-		| TClassDecl _ -> "TClassDecl"
-		| TEnumDecl _ -> "TEnumDecl"
-		| TTypeDecl _ -> "TTypeDecl"
-		| TAbstractDecl _ -> "TAbstractDecl"
-
-let module_type_s t =
-	(module_type_type t) ^ " " ^ (module_type_path t)
-	
-let to_real_path s =
-	match s with
-		| "Void" -> "()"
-		| _ -> s
-	
-let rec module_type_ps_s md_t ps =
-	(match ps with
-		| [] -> to_real_path (module_type_path md_t)
-		| _ ->
-			let s = String.concat ", " (List.map type_s ps) in
-			(to_real_path (module_type_path md_t)) ^ "<" ^ s ^ ">")
-
-and type_s t =
-	match t with
-		| TMono t ->
-			(match !t with
-				| Some mt -> type_s mt
-				| None -> "$TMono")
-		| TEnum(e, ps) ->
-			module_type_ps_s (TEnumDecl e) ps
-		| TInst(cl, ps) ->
-			module_type_ps_s (TClassDecl cl) ps
-		| TType(d, ps) ->
-			module_type_ps_s (TTypeDecl d) ps
-		| TFun(args, ret) ->
-			"$TFun"
-		| TAnon a ->
-			"$TAnon"
-		| TDynamic t ->
-			"$TDynamic"
-		| TLazy t -> 
-			"$TLazy"
-		| TAbstract(a, ps) ->
-			module_type_ps_s (TAbstractDecl a) ps
-			
-let unwrap_paren e =
-	match e.eexpr with
-		| TParenthesis e' -> e'
-		| _ -> e
-		
 let configure gen =
+
+	(* Use 2 spaces instead of tabs /* burn me */ *)
+	let push_indent w = w.sw_indents <- "  "::w.sw_indents; w.sw_indent <- String.concat "" w.sw_indents in
+
+	let pop_indent w =
+		match w.sw_indents with
+			| h::tail -> w.sw_indents <- tail; w.sw_indent <- String.concat "" w.sw_indents
+			| [] -> w.sw_indent <- "/*?*/"
+	in
 
 	let begin_block w = write w "{"; push_indent w; newline w; in
 	let end_block w = pop_indent w; (if w.sw_has_content then newline w); write w "}"; newline w; in
@@ -104,6 +48,76 @@ let configure gen =
 		Buffer.contents b
 	in
 
+	let path_s t =
+			let (ns, n) =
+				(match t with
+				| TClassDecl cl -> cl.cl_path
+				| TEnumDecl e -> e.e_path
+				| TTypeDecl d -> d.t_path
+				| TAbstractDecl a -> a.a_path) in
+			String.concat "." (ns @ [n])
+	in
+	
+	let to_real_path s =
+		match s with
+			| "Void" -> "()"
+			| _ -> s
+	in
+		
+	let rec path_param_s md_t ps =
+		(match ps with
+			| [] -> to_real_path (path_s md_t)
+			| _ ->
+				let s = String.concat ", " (List.map t_s ps) in
+				(to_real_path (path_s md_t)) ^ "<" ^ s ^ ">")
+	
+	and t_s t =
+		match t with
+			| TMono t ->
+				(match !t with
+					| Some mt -> t_s mt
+					| None -> "$TMono")
+			| TEnum(e, ps) ->
+				path_param_s (TEnumDecl e) ps
+			| TInst ({ cl_kind = KTypeParameter _; cl_path = (ns,n) }, []) ->
+				if List.length ns >= 1 then
+					let parent_p =
+						let rev = List.rev ns in
+						let parent_ns = List.rev (List.tl rev) in
+						let parent_n = List.hd rev in
+						(parent_ns, parent_n)
+					in
+					if Hashtbl.mem gen.gtypes parent_p then
+						match Hashtbl.find gen.gtypes parent_p with
+							| TClassDecl({ cl_interface = true }) ->
+								String.concat "_" (ns @ [n])
+							| _ -> n
+					else
+						n
+				else
+					n
+			| TInst(cl, ps) ->
+				path_param_s (TClassDecl cl) ps
+			| TType(d, ps) ->
+				path_param_s (TTypeDecl d) ps
+			| TFun(args, ret) ->
+				"$TFun"
+			| TAnon a ->
+				"$TAnon"
+			| TDynamic t ->
+				"$TDynamic"
+			| TLazy t -> 
+				"$TLazy"
+			| TAbstract(a, ps) ->
+				path_param_s (TAbstractDecl a) ps
+	in
+				
+	let unwrap_paren e =
+		match e.eexpr with
+			| TParenthesis e' -> e'
+			| _ -> e
+	in
+
 	let in_value = ref false in
 	
 	let in_function_decl = ref false in
@@ -133,8 +147,7 @@ let configure gen =
 						| TSuper -> write w "super")
 				| TLocal v -> write_id w v.v_name
 				| TArray(e1, e2) ->
-					(* array access e1[e2] *)
-					write w "TArray"
+					expr_s w e1; write w "["; expr_s w e2; write w "]"
 				| TBinop(op, e1, e2) ->
 					expr_s w e1;
 					write w (" " ^ (Ast.s_binop op) ^ " ");
@@ -145,10 +158,10 @@ let configure gen =
 					write_field w (field_name fa)
 				| TTypeExpr(md_t) ->
 					(match md_t with
-						| TClassDecl cl -> write w (type_s (TInst(cl, List.map (fun _ -> t_dynamic) cl.cl_params)))
-						| TEnumDecl en -> write w (type_s (TEnum(en, List.map (fun _ -> t_dynamic) en.e_params)))
-						| TTypeDecl td -> write w (type_s (gen.gfollow#run_f (TType(td, List.map (fun _ -> t_dynamic) td.t_params))))
-						| TAbstractDecl a -> write w (type_s (TAbstract(a, List.map (fun _ -> t_dynamic) a.a_params)))
+						| TClassDecl cl -> write w (t_s (TInst(cl, List.map (fun _ -> t_dynamic) cl.cl_params)))
+						| TEnumDecl en -> write w (t_s (TEnum(en, List.map (fun _ -> t_dynamic) en.e_params)))
+						| TTypeDecl td -> write w (t_s (gen.gfollow#run_f (TType(td, List.map (fun _ -> t_dynamic) td.t_params))))
+						| TAbstractDecl a -> write w (t_s (TAbstract(a, List.map (fun _ -> t_dynamic) a.a_params)))
 					)
 				| TParenthesis e ->
 					write w "("; expr_s w e; write w ")"
@@ -174,7 +187,7 @@ let configure gen =
 				| TVar (v, e_op) ->
 					write w "var ";
 					write_id w v.v_name;
-					print w ": %s" (type_s v.v_type);
+					print w ": %s" (t_s v.v_type);
 					(match e_op with
 						| None ->
 							write w " = ";
@@ -260,10 +273,33 @@ let configure gen =
 		expr_s w e
 	in
 
+	let get_string_params cl_params =
+		match cl_params with
+			| [] ->
+				("","")
+			| _ ->
+				let params = Printf.sprintf "<%s>" (String.concat ", " (List.map (fun (_, tcl) -> match follow tcl with | TInst(cl, _) -> snd cl.cl_path | _ -> assert false) cl_params)) in
+				let params_extends = List.fold_left (fun acc (name, t) ->
+					match run_follow gen t with
+						| TInst (cl, p) ->
+							(match cl.cl_implements with
+								| [] -> acc
+								| _ -> acc) (* TODO
+								| _ -> (sprintf " where %s : %s" name (String.concat ", " (List.map (fun (cl,p) -> path_param_s (TClassDecl cl) cl.cl_path p) cl.cl_implements))) :: acc ) *)
+						| _ -> trace (t_s t); assert false (* FIXME it seems that a cl_params will never be anything other than cl.cl_params. I'll take the risk and fail if not, just to see if that confirms *)
+				) [] cl_params in
+				(params, String.concat " " params_extends)
+	in
+	
 	let rec gen_class_field w is_static cl cf =
 		let static_s = if is_static then "class " else "" in
-		let public_s = "" in
+		let public_s =
+			if cl.cl_interface then ""
+			else (if cf.cf_public then "public " else "internal ") in
 		let name_s = cf.cf_name in
+		let (generic_s, _) = get_string_params cf.cf_params in
+		let is_override = List.memq cf cl.cl_overrides in
+		let override_s = if is_override then "override " else "" in
 		(*Printf.printf "%s\n" (Std.dump cf)*)
 		match cf.cf_kind with
 			| Method MethNormal ->
@@ -271,10 +307,10 @@ let configure gen =
 					| TFun(args, ret) ->
 						let func_s = "func " in
 						let args_s = String.concat ", " (List.map (fun (argname, argopt, argt) ->
-							argname ^ ": " ^ (type_s argt)
+							argname ^ ": " ^ (t_s argt)
 						) args) in
-						let ret_s = type_s ret in
-						write w (static_s ^ func_s ^ public_s ^ name_s ^ "(" ^ args_s ^ ")" ^ " -> " ^ ret_s ^ " ");
+						let ret_s = t_s ret in
+						write w (override_s ^ public_s ^ static_s ^ func_s ^ name_s ^ generic_s ^ "(" ^ args_s ^ ")" ^ " -> " ^ ret_s ^ " ");
 						(*begin_block w;*)
 						(match cf.cf_expr with
 							| Some e ->
@@ -288,21 +324,90 @@ let configure gen =
 					| _ -> raise (SwiftEx "why u no TFun"))
 			| _ -> ()
 	in
-		
+	
 	let gen_class w cl =
-    write w ("class " ^ (module_type_path (TClassDecl cl)) ^ " ");
-    begin_block w;
-  	List.iter (gen_class_field w true cl) cl.cl_ordered_statics;
-  	List.iter (gen_class_field w false cl) cl.cl_ordered_fields;
-    end_block w
+		let public_s = if cl.cl_private then "internal " else "public " in
+		let class_s = if cl.cl_interface then "protocol " else "class " in
+		let generic_s =
+			if cl.cl_interface then
+				""
+			else
+				let (generic_s, _) = get_string_params cl.cl_params in
+				generic_s
+		in
+		
+		let extends_implements_l =
+			(match cl.cl_super with
+				| None -> cl.cl_implements
+				| Some x -> x::cl.cl_implements)
+		in
+
+		let extends_implements_s =
+      		(match extends_implements_l with
+      			| [] -> ""
+      			| _ -> ": " ^ (String.concat ", " (List.map (fun (cl, ps) ->
+        			if cl.cl_interface then
+          				path_param_s (TClassDecl cl) []
+        			else
+          				path_param_s (TClassDecl cl) ps	
+				) extends_implements_l)))
+		in
+		write w (public_s ^ class_s ^ (path_s (TClassDecl cl)) ^ generic_s ^ extends_implements_s ^ " ");
+		begin_block w;
+		(* For interfaces, we need to declare associated types for each generic parameters.
+		 * e.g. given `interface A<T>`, we need to transform this to
+		 * ```
+		 * protocol A {
+	     *   typealias A_T
+		 * }
+		 * ```
+		 * To avoid name collisions, the whole type path is included in the typealias name,
+		 * connected using '_'.
+		 *)
+		if cl.cl_interface then
+			(let (ns, n) = cl.cl_path in
+			List.iter (fun (name, t) ->
+				let alias_s = (String.concat "_" (ns @ [n] @ [name])) in
+				write w ("typealias " ^ alias_s);
+				newline w 
+			) cl.cl_params);
+		(* If the current class (interface) extends or implements a generic interface,
+		 * we need to add typealias declarations. e.g. given `If class B<T> implements IA<T>`,
+		 * we need to transform this to:
+		 * ```
+		 * class B<T> : IA {
+		 *   typealias IA_T = T
+		 *   ....
+		 * }
+		 * ```
+		 *)
+		List.iter (fun (cl, ps) ->
+			if cl.cl_interface then begin
+				let (ns, n) = cl.cl_path in
+				List.iter (fun ((name, t), t_in_cl) ->
+					let alias_s = (String.concat "_" (ns @ [n] @ [name])) in
+					write w ("typealias " ^ alias_s ^ " = " ^ (t_s t_in_cl));
+					newline w
+				) (List.combine cl.cl_params ps);
+			end
+		) extends_implements_l;
+		List.iter (gen_class_field w true cl) cl.cl_ordered_statics;
+		List.iter (gen_class_field w false cl) cl.cl_ordered_fields;
+		end_block w
 	in
 
-  let gen_enum w e =
-  	write w ("enum " ^ (module_type_path (TEnumDecl e)))
+	let gen_enum w e =
+	  	write w ("enum " ^ (path_s (TEnumDecl e)))
 	in
 
 	let module_type_gen w md_tp =
-		if module_type_path md_tp = "Hello" || module_type_path md_tp = "Guid" then begin
+		if (path_s md_tp = "Hello"
+			|| path_s md_tp = "Guid"
+			|| path_s md_tp = "SubHello"
+			|| path_s md_tp = "IHello"
+			|| path_s md_tp = "IHelloT"
+			|| path_s md_tp = "SubHelloT"
+				) then begin
 			match md_tp with
 				| TClassDecl cl ->
 					if not cl.cl_extern then begin
--- tmp_tests/Hello.hx
+++ tmp_tests/Hello.hx
@@ -1,14 +1,43 @@
 package ;
 
-class Hello {
-  function sayHi(name: String): String {
+interface IHello {
+  function sayHi(name: String): String;
+}
+
+interface IHelloT<T> extends IHello {
+  function identity(t: T): T;
+}
+
+class Hello implements IHello {
+  public function sayHi(name: String): String {
     return 'hi $name';
   }
-	static function allSayHi(names: Array<String>): String {
-		return 'hi ${names.join(" and ")}';
+	public static function allSayHi(names: Array<String>): String {
+		return 'hi ${names[0]}';
 	}
-  static function main() {
+  public static function main() {
 
+  }
+  public static function identityStaticAnyways<T>(t: T): T {
+    return t;
+  }
+  public function identityAnyways<T>(t: T): T {
+    return t;
+  }
+}
+
+class SubHello extends Hello implements IHello {
+  override public function sayHi(name: String): String {
+    return 'subhi $name';
+  }
+}
+
+class SubHelloT<T> extends SubHello implements IHelloT<T> {
+  public function identity(t: T): T {
+    return t;
+  }
+  public static function subIdentityStaticAnyways<T>(t: T): T {
+    return t;
   }
 }
 
